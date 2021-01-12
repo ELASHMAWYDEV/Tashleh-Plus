40d1bdefbb44913c1d9eca90959d1c18faac6112
ELASHMAWY DEV <contact@elashmawydev.com>
2021-01-12 19:56:14 +0200

    Added react routing to node app

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const cors = require("cors");
 const PORT = process.env.PORT || 5005;
+
+
+//Middlewares
+app.use(express.json());
+app.use(cors());
+
+
+
+//For the react app
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
